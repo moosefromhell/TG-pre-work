346824eab1dfcc80cca67265465fdd87cb3e169a
Moose <joshua.archer94@gmail.com>
2018-08-07 15:00:36 -0600

    functions

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-let calculatorIsOn = false;
-
-const pressPowerButton = () => {
-  if (calculatorIsOn) {
-    console.log('Calculator turning off.');
-    calculatorIsOn = false;
-  } else {
-    console.log('Calculator turning on.');
-    calculatorIsOn = true;
-  }
-};
-
-pressPowerButton();
-// Output: Calculator turning on.
-
-pressPowerButton();
-// Output: Calculator turning off.
+const takeOrder = () => {
+  console.log('Order; pizza')
+}
+takeOrder();
