b6a72f78d5f9970d5f7d54001b15089013712df9
Moose <joshua.archer94@gmail.com>
2018-08-07 13:26:01 -0600

    using exclamation points to evaluate if something is false

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ if (wordCount) {
 
 let favoritePhrase = '1';
 
-if (favoritePhrase) {
+if (!favoritePhrase) {
   console.log("This string doesn't seem to be empty.");
 } else {
   console.log('This string is definitely empty.');
