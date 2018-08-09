e88abd250d1da6a73fe5c8e6d05ec38d2607a77c
Moose <joshua.archer94@gmail.com>
2018-08-09 16:59:08 -0600

    for loops

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 let vacationSpots = ["Norway", "Japan", "Canada"];
-console.log(vacationSpots[0]);
-console.log(vacationSpots[1]);
-console.log(vacationSpots[2]);
+for (let vacationSpotIndex = 0; vacationSpotIndex < vacationSpots.length; vacationSpotIndex++) {
+  	console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
+}
