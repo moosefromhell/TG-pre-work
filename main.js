e69b3f4b7eb26c70c3eacfb1d1e6c89afb2d91b7
Moose <joshua.archer94@gmail.com>
2018-08-09 14:02:23 -0600

    used length

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ let newYearsResolutions = ['Get a tech job', 'Learn a new language', 'Kick John'
 let listItem = (newYearsResolutions[1])
 	console.log(newYearsResolutions[2])
 	console.log(newYearsResolutions[3])
+console.log(newYearsResolutions.length);
