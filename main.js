c3fa857b7d103da2ca4551492e7a1a95dc20b6c8
Moose <joshua.archer94@gmail.com>
2018-08-09 13:43:56 -0600

    property access

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 let newYearsResolutions = ['Get a tech job', 'Lose 50lbs', 'Kick John']
 console.log(newYearsResolutions)
+let listItem = (newYearsResolutions[0])
+console.log(newYearsResolutions[2])
+console.log(newYearsResolutions[3])
