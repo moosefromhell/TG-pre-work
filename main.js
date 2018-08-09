7db8fe4cb8d9415fe487e40244b60ca012822b77
Moose <joshua.archer94@gmail.com>
2018-08-09 13:39:29 -0600

    adding arrays

--- main.js
+++ main.js
@@ -1,3 +1,2 @@
-let bucketList = ['Rappel into a cave', 'Take a falconry class', 'Learn to juggle'];
-
-console.log(bucketList);
+let newYearsResolutions = ['Get a tech job', 'Lose 50lbs', 'Kick John']
+console.log(newYearsResolutions)
