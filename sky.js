3802566afe9a0e7af49f208dc727e8519b28b72d
Moose <joshua.archer94@gmail.com>
2018-08-09 12:18:33 -0600

    global scope 2

--- sky.js
+++ sky.js
@@ -4,6 +4,8 @@ const galaxy = "The Milky Way"
 let stars = 'North Star'
 
 const myNightSky = () => {
+  stars = 'Sirius';
   return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
 };
 console.log(myNightSky())
+console.log(stars)
