126797661c5e460734ec308d2581e30585df0263
Moose <joshua.archer94@gmail.com>
2018-08-09 14:54:14 -0600

    arrays using let and const

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
-//let groceryList =
-groceryList.shift();
-groceryList.unshift('popcorn');
-console.log(groceryList.slice(0, 3, 4, 5, 6));
-console.log(groceryList);
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+condiments.push('mayo');
+condiments = ['mayo'];
+utensils.pop(3);
+utensils = ['spoon']
