5de4381167d303b05d2ab91a290262d357058425
Moose <joshua.archer94@gmail.com>
2018-08-10 16:15:36 -0600

    while loop

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-let myPlaces = ['japan', 'canada', 'norway'];
-
-let friendPlaces = ['canada', 'australia', 'bostan'];
-
-for (let myPlacesIndex = 0; myPlacesIndex < myPlaces.length; myPlacesIndex++) {
-  for (let friendPlacesIndex = 0; friendPlacesIndex < friendPlaces.length; friendPlacesIndex++) {
-		if (myPlaces[myPlacesIndex] === friendPlaces[friendPlacesIndex]) {
-      console.log(friendPlaces[friendPlacesIndex]);
-    }
-  }
-}
+let cards = ['Diamond', 'Spade', 'Heart', 'Club'];
+let currentCard = 'Heart'
+while (currentCard !== 'Spade') {
+  console.log(currentCard)
+  currentCard = cards[Math.floor(Math.random() * 4)];
+} console.log('found a spade')
