4b1ae3bfc8867d3e33c6a89f96baaa3906add3db
Moose <joshua.archer94@gmail.com>
2018-08-10 20:40:14 -0600

    getters and setters

--- main.js
+++ main.js
@@ -1,36 +1,14 @@
 let person = {
-  name: 'Moose',
-  age: 23,
-  weekendAlarm: 'No alarms needed',
-  weekAlarm: 'Alarm set to 7AM',
-  sayHello: function() {
-    return `Hello, my name is ${this.name}`;
-  },
-  sayGoodbye() {
-  return 'Goodbye!';
-}
+  _name: 'Lu Xun',
+  _age: 137,
+
+  set age(ageInput) {
+    if (typeof ageInput === 'number') {
+      this._age = ageInput;
+    }
+    else {
+      console.log('Invalid input');
+      return 'Invalid input';
+    }
+  }
 };
-let friend = {
-  name: 'John'
-}
-friend.sayHello = person.sayHello;
-console.log(person.sayHello())
-person.hobbies = ['Drumming', 'Cooking'];
-person.hobbies = ['Drumming'];
-console.log(person.hobbies);
-
-
-console.log(person['name']);
-console.log(person['age']);
-
-let day = 'Tuesday';
-let alarm;
-
-if (day === 'Saturday' || day === 'Sunday' ) {
-  alarm = 'weekendAlarm';
-} else {
-  alarm = 'weekAlarm';
-}
-
-console.log(person[alarm]);
-console.log(friend.sayHello())
