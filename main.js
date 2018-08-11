a3d9139a4b7061c0f6cd9e63732de2620265d2be
Moose <joshua.archer94@gmail.com>
2018-08-10 19:06:16 -0600

    accessing object properties III

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 let person = {
   name: 'Moose',
-  age: 23
+  age: 23,
+  weekendAlarm: 'No alarms needed',
+  weekAlarm: 'Alarm set to 7AM'
+
 };
+if (day === 'Saturday' || day === 'Sunday' ) {
+  alarm = 'weekendAlarm';
+} else {
+  alarm = 'weekAlarm';
+}
+let day = 'Friday';
+let alarm;
 console.log(person['name'])
 console.log(person['age'])
+console.log(person[alarm]);
