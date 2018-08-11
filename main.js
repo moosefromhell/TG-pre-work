d99c46f4f348355b08aae9a8a90965f934daea99
Moose <joshua.archer94@gmail.com>
2018-08-10 19:20:12 -0600

    adding a property

--- main.js
+++ main.js
@@ -2,16 +2,19 @@ let person = {
   name: 'Moose',
   age: 23,
   weekendAlarm: 'No alarms needed',
-  weekAlarm: 'Alarm set to 7AM'
+  weekAlarm: 'Alarm set to 7AM',
+
 
 };
-if (day === 'Saturday' || day === 'Sunday' ) {
+let day = 'Friday';
+person.hobbies = ['drumming', 'cooking'];
+console.log(person.hobbies)
+if (day === 'Saturday' || day === 'Sunday') {
   alarm = 'weekendAlarm';
 } else {
   alarm = 'weekAlarm';
-}
-let day = 'Friday';
+//let day = 'Friday';
 let alarm;
-console.log(person['name'])
-console.log(person['age'])
+console.log(person['name']);
+console.log(person['age']);
 console.log(person[alarm]);
