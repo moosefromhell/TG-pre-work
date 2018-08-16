905f761c3ab68b7f438d32666e8a74266b759983
Moose <joshua.archer94@gmail.com>
2018-08-16 17:56:39 -0600

    generated json package

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
-npm WARN project@1.0.0 No repository field.
-$ npm run build
 
-> project@1.0.0 build /home/ccuser/workspace/le
-arn-javascript-transpilation-bct-transpilation-
-with-babel
-> babel src -d lib
-
-src/main.js -> lib/main.js
-$ cat lib/main.js
-"use strict";
-
-var pasta = "Spaghetti"; // ES5 syntax
-
-var meat = "Pancetta"; // ES6 syntax
-
-var sauce = "Eggs and cheese"; // ES6 syntax
-
-// Template literals, like the one below, were
-introduced in ES6
-var carbonara = "You can make carbonara with "
-+ pasta + ", " + meat + ", and a sauce made wit
-h " + sauce + ".";$
+$ cat package.json
+{
+"name": "learning-babel",
+"version": "1.0.0",
+"description": "Use babel to transpile",
+"main": "index.js",
+"scripts": {
+  "test": "echo \"Error: no test specified\"
+&& exit 1"
+},
+"author": "Moose",
+"license": "ISC"
+}
+$
