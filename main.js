bbd6921d8a0f8df816c1d2a6cb18e4bc39e019cb
Moose <joshua.archer94@gmail.com>
2018-08-16 18:13:05 -0600

    .babelrc

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-npm WARN optional SKIPPING OPTIONAL DEPENDENCY:
- fsevents@^1.0.0 (node_modules/chokidar/node_mo
-dules/fsevents):
-npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: U
-nsupported platform for fsevents@1.2.4: wanted
-{"os":"darwin","arch":"any"} (current: {"os":"l
-inux","arch":"x64"})
-npm WARN learning-babel@1.0.0 No repository fie
-ld.
-$ ls
-node_modules  package.json  src
-$
+$ touch .babelrc
