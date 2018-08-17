9541b1c2908e4c70e7103aecda5720cd232a13d7
Moose <joshua.archer94@gmail.com>
2018-08-16 19:05:03 -0600

    using require()

--- 1-airplane.js
+++ 1-airplane.js
@@ -1,3 +1,5 @@
-let Airplane = {};
-Airplane.myAirplane = "StarJet"
-module.exports = Airplane;
+const Airplane = require('./1-airplane.js');
+function displayAirplane() {
+  console.log(Airplane.myAirplane);
+}
+displayAirplane()
