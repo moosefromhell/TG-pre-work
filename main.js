f90c343188b829ba6b5908bb373078d25db2f015
Moose <joshua.archer94@gmail.com>
2018-08-16 18:06:33 -0600

    installing node packages

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-
-$ cat package.json
-{
-"name": "learning-babel",
-"version": "1.0.0",
-"description": "Use babel to transpile",
-"main": "index.js",
-"scripts": {
-  "test": "echo \"Error: no test specified\"
-&& exit 1"
-},
-"author": "Moose",
-"license": "ISC"
-}
+npm WARN optional SKIPPING OPTIONAL DEPENDENCY:
+ fsevents@^1.0.0 (node_modules/chokidar/node_mo
+dules/fsevents):
+npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: U
+nsupported platform for fsevents@1.2.4: wanted
+{"os":"darwin","arch":"any"} (current: {"os":"l
+inux","arch":"x64"})
+npm WARN learning-babel@1.0.0 No repository fie
+ld.
+$ ls
+node_modules  package.json  src
 $
