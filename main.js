d58d8d90f8fd7af210e8434870c029e7f9512b72
Moose <joshua.archer94@gmail.com>
2018-08-17 13:53:06 -0600

    http requests

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
-npm run build
-npm ERR! Linux 4.4.0-1038-aws
-npm ERR! argv "/usr/bin/nodejs" "/usr/bin/npm"
-"run" "build"
-npm ERR! node v7.10.1
-npm ERR! npm  v4.2.0
-npm ERR! file /home/ccuser/workspace/learn-java
-script-transpilation-bct-npm-init/package.json
-npm ERR! code EJSONPARSE
-
-npm ERR! Failed to parse json
-npm ERR! Unexpected token 's' at 14:6
-npm ERR!     "scripts": {
-npm ERR!      ^
-npm ERR! File: /home/ccuser/workspace/learn-jav
-ascript-transpilation-bct-npm-init/package.json
-npm ERR! Failed to parse package.json data.
+console.log('First message!');
+setTimeout(() => {
+   console.log('This message will always run last...');
+}, 0);
+console.log('Second message!');
