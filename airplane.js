0afba9834833155037faa9ff25ecddd27dda932c
Moose <joshua.archer94@gmail.com>
2018-08-17 12:29:17 -0600

    more named imports

--- airplane.js
+++ airplane.js
@@ -1,26 +1,14 @@
+import {availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element){
+   console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  });
 
- let availableAirplanes = [
-  {
-    name: 'AeroJet',
-    fuelCapacity: 800,
-    availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators']
-  },
-  {
-    name: 'SkyJet',
-    fuelCapacity: 500,
-    availableStaff: ['pilots', 'flightAttendants']
-  }
-];
-let flightRequirements = {
-  requiredStaff: 4,
-};
-function
-meetsStaffRequirements(availableStaff, requiredStaff) {
-  if (availableStaff.length >= requiredStaff)
-    {
-      return true;
-    } else {
-      return false;
-    }
-};
-export { availableAirplanes, flightRequirements, meetsStaffRequirements};
+}
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element){
+    console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
+displayFuelCapacity();
+displayStaffStatus();
