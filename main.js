4555499363ba4c2359c80eab5e25c217fc282fbd
Moose <joshua.archer94@gmail.com>
2018-08-16 18:24:09 -0600

    build

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-$ touch .babelrc
+npm run build
+npm ERR! Linux 4.4.0-1038-aws
+npm ERR! argv "/usr/bin/nodejs" "/usr/bin/npm"
+"run" "build"
+npm ERR! node v7.10.1
+npm ERR! npm  v4.2.0
+npm ERR! file /home/ccuser/workspace/learn-java
+script-transpilation-bct-npm-init/package.json
+npm ERR! code EJSONPARSE
+
+npm ERR! Failed to parse json
+npm ERR! Unexpected token 's' at 14:6
+npm ERR!     "scripts": {
+npm ERR!      ^
+npm ERR! File: /home/ccuser/workspace/learn-jav
+ascript-transpilation-bct-npm-init/package.json
+npm ERR! Failed to parse package.json data.
