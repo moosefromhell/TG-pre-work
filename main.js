1dec447eb7a8f6500913dfc622b8025f67e18b3a
Moose <joshua.archer94@gmail.com>
2018-08-17 16:50:12 -0600

    fetch() GET Requests IV

--- main.js
+++ main.js
@@ -19,7 +19,9 @@ const getSuggestions = () => {
     throw new Error('Request failed!');
   }, networkError => {
     console.log(networkError.message)
-  })
+  }).then(jsonResponse => {
+    renderResponse(jsonResponse);
+  } );
 }
 
 // Clears previous results and display results to webpage
