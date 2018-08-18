de436e773052b7598599a178666b5f24c5058588
Moose <joshua.archer94@gmail.com>
2018-08-18 12:44:33 -0600

    async POST Requests II

--- main.js
+++ main.js
@@ -1,37 +1,15 @@
-// Information to reach API
-const url = 'https://api.datamuse.com/words?';
-const queryParams = 'rel_jja=';
-
-// Selecting page elements
-const inputField = document.querySelector('#input');
-const submit = document.querySelector('#submit');
-const responseField = document.querySelector('#responseField');
-
-// AJAX function
-const getSuggestions = async () => {
-  const wordQuery = inputField.value;
-  const endpoint = `${url}${queryParams}${wordQuery}`
+const getData = async () => {
   try {
-  const response = await fetch(endpoint);
-    if(response.ok){
-      const jsonResponse = await
-      response.json()
-      renderResponse(jsonResponse);
+    const response = await fetch('https://api-to-call.com/endpoint', {
+      method: 'POST',
+      body: JSON.stringify({id: 200}),
+    });
+    if (response.ok) {
+    	const jsonResponse = await response.json();
+      return jsonResponse
     }
-} catch (error) {
-  console.log(error)
-}
-}
-// Code goes here
-
-
-// Clear previous results and display results to webpage
-const displaySuggestions = (event) => {
-  event.preventDefault();
-  while(responseField.firstChild){
-    responseField.removeChild(responseField.firstChild)
+    throw new Error('Request failed!')
+  } catch (error) {
+  	console.log(error)
   }
-  getSuggestions();
 }
-
-submit.addEventListener('click', displaySuggestions);
